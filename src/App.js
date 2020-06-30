c880e3f094fccfc093b1732447ed9b9011bb0922
sasaki kei <sasakei1021@icloud.com>
2020-06-30 17:32:24 +0900

    useContext

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import logo from "./logo.svg";
 import "./App.css";
 import Basic1 from "./components/Basic1";
 import Fetch from "./components/Fetch";
+import AppContext from "./contexts/AppContexts";
+import B from "./components/B";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Basic1 name="佐々木だよ" />
-        <Fetch />
-      </header>
-    </div>
+    <AppContext.Provider value={"value from App.js"}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Basic1 name="佐々木だよ" />
+          <Fetch />
+          <B />
+        </header>
+      </div>
+    </AppContext.Provider>
   );
 }
 
