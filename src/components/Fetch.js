17a1dd9d2415470b2b3e2107c7bfb1b1b8c6eb8c
sasaki kei <sasakei1021@icloud.com>
2020-06-30 17:16:54 +0900

    fetch-practice

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -2,21 +2,32 @@ import React, { useState, useEffect } from "react";
 
 const Fetch = () => {
   const [posts, setPosts] = useState([]);
+  const [id, setId] = useState(1);
+  const [clicked, setClicked] = useState(false);
+  const handlerClicked = () => {
+    setClicked(!clicked);
+  };
+
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts", { method: "GET" })
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, { method: "GET" })
       .then((res) => res.json())
       .then((data) => {
         setPosts(data);
       });
-  }, []);
+  }, [clicked]);
 
   return (
     <div>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
+      <input
+        type="text"
+        value={id}
+        onChange={(evt) => setId(evt.target.value)}
+      />
+      <button type="button" onClick={handlerClicked}>
+        {" "}
+        GetPost{" "}
+      </button>
+      {posts.title}
     </div>
   );
 };
